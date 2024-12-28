55e42222c11100fce227d31137a66101f3bc6546
qwavy <nursultanr585@gmail.com>
2024-12-28 21:11:20 +0500

    feat:codee

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+
+
+
+  if(!options.repeatTimes){
+    return str += options.addition
+  }
+
+  if(!options.additionSeparator){
+    options.additionSeparator = "|"
+  }
+
+  if(!options.separator){
+    options.separator = "+"
+  }
+
+  if(!options.additionRepeatTimes && options.addition){
+    options.additionRepeatTimes = 1
+  }
+
+  if(typeof options.separator !== "string"){
+    options.separator = String(options.separator)
+  }
+  if(typeof options.addition !== "string"){
+    options.addition = String(options.addition)
+  }
+  if(typeof options.additionSeparator !== "string"){
+    options.additionSeparator = String(options.additionSeparator)
+  }
+
+  let result = ""
+  for (let i = 0; i < options.repeatTimes; i++) {
+      let myStr = str
+      for (let j = 0; j < options.additionRepeatTimes; j++) {
+          myStr += options.addition
+          if (j < options.additionRepeatTimes - 1) {
+              myStr += options.additionSeparator
+          }
+      }
+      if(i < options.repeatTimes - 1){
+          myStr += options.separator
+      }
+      result += myStr
+  }
+
+  return result
 }
 
 module.exports = {
