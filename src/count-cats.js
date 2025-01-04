621ecd2bbfeaaae331e3e219a5cd435ac2869501
qwavy <nursultanr585@gmail.com>
2025-01-04 21:48:46 +0500

    feat: counct cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let count = 0
+  for(let rows = 0;rows < matrix.length;rows++){
+      for(let cols = 0;cols < matrix[rows].length;cols++){
+          if(matrix[rows][cols] === "^^"){
+              count++
+          }
+      }
+  }
+
+  return count
 }
 
 module.exports = {
